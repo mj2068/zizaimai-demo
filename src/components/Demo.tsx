4910456956ce997e109417aa4e8729752542eac5
mj2068 <yuelianghushenfu@sina.com>
2024-09-22 20:55:44 +0800

    add simple a links

---
+++ src/components/Demo.tsx
@@ -0,0 +1,18 @@
+import { Space } from "antd";
+
+export default function Demo() {
+  //
+
+  return (
+    <>
+      <p>作品</p>
+      <div>
+        <Space direction="vertical">
+          <a href="fitting-room">3D换装试衣间</a>
+          <a href="mystery-door">奇异门入口动画页面</a>
+          <a href="room-scene">日式室内场景</a>
+        </Space>
+      </div>
+    </>
+  );
+}
