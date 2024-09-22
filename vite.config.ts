acd26b6858a6fa8fd25084b52a60eb2e99cf2bef
mj2068 <yuelianghushenfu@sina.com>
2024-09-22 21:51:41 +0800

    change vite confing to base: /demo/

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/demo/",
   resolve: {
     // alias: {
     //   "/src": fileURLToPath(new URL("./src", import.meta.url)),
